d98cbe2404e72482b05c1be9beebf817ade66529
debussy <fynn@fynnyoung.com>
2022-06-21 14:22:50 +0200

    edited and simplified

--- load_dataset/custom_MOT.py
+++ load_dataset/custom_MOT.py
@@ -1,44 +1,29 @@
 import os
-import PIL
-import torch
-import numpy as np
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
-import torchvision
+from skimage import io
 
-# Set seeds
-np.random.seed(0)
-torch.manual_seed(0)
-torch.cuda.manual_seed(0)
-torch.cuda.manual_seed_all(0)
+class custom_MOT(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
 
-class MOTData(Dataset):
-    def __init__(self, setname):
-        self.setname = setname
-        assert setname in ['train','val','test']
-        
-        # Define dataset
-        overall_dataset_dir = os.path.join(os.path.join(os.getcwd(),'load_dataset'), 'MOT_data')
-        self.selected_dataset_dir = os.path.join(overall_dataset_dir,setname)
-        
-        self.all_filenames = os.listdir(self.selected_dataset_dir)
-        self.all_labels = pd.read_csv(os.path.join(overall_dataset_dir,'MOT_labels.csv'),header=0,index_col=0)
-        self.label_meanings = self.all_labels.columns.values.tolist()
-    
     def __len__(self):
-        return len(self.all_filenames)
-        
+        return len(self.data)
+
     def __getitem__(self, idx):
-        selected_filename = self.all_filenames[idx]
-        imagepil = PIL.Image.open(os.path.join(self.selected_dataset_dir,selected_filename)).convert('RGB')
-        
-        # convert image to Tensor
-        image = torchvision.transforms.ToTensor(imagepil)
-        
-        # load label
-        label = torch.Tensor(self.all_labels.loc[selected_filename,:].values)
-        
-        sample = {'data':image,
-                  'label':label,
-                  'img_idx':idx}
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.data.iloc[idx, 0])
+        image = io.imread(img_name)
+        label = self.data.iloc[idx, 1]
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
         return sample
