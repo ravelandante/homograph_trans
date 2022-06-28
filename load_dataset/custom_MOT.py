cb3bd7f667b9038e8f0410ea93b9fb83ed871d4f
debussy <fynn@fynnyoung.com>
2022-06-28 11:07:07 +0200

    conversion of label to array/tensor

--- load_dataset/custom_MOT.py
+++ load_dataset/custom_MOT.py
@@ -3,6 +3,8 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from skimage import io
+from torchvision import transforms
+import numpy as np
 
 class custom_MOT(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
@@ -14,13 +16,17 @@ class custom_MOT(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        #if torch.is_tensor(idx):
+        #    idx = idx.tolist()
+
+        transform = transforms.Compose([transforms.ToTensor()])
 
         img_name = os.path.join(self.root_dir, self.data.iloc[idx, 0])
         image = io.imread(img_name)
-        label = self.data.iloc[idx, 1]
-        sample = {'image': image, 'label': label}
+        label = np.array(self.data.iloc[idx, 1].replace('#', ',').split(',')).astype(float)     # string from csv to 1D array
+        label = np.reshape(label, (3, 3))                                                       # reshape array into 3x3 transformation matrix
+        image = transform(image)
+        sample = [image, label]
 
         if self.transform:
             sample = self.transform(sample)
